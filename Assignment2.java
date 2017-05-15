e355e015941f58bcf120fb973acb52dba69ccc31
cahaskins <cahaskins@cpp.edu>
2017-05-15 11:27:39 -0700

    Program will now run indefinitely until told to exit. Can run multiple root locating methods.

--- Assignment2.java
+++ Assignment2.java
@@ -16,8 +16,8 @@ public class Assignment2 {
     private static Equation problemB = new HyperbolicB();
     private static Equation problemBPrime = new HyperbolicBPrime();
 
-    private static Equation currProblem = problemA;
-    private static Equation currProblemPrime = problemAPrime;
+    private static Equation currProblem;
+    private static Equation currProblemPrime;
 
     // constants for Problem (a)
     private static final double BISECTION_A1_P1 = 0;
@@ -31,56 +31,87 @@ public class Assignment2 {
     private static final double NEWTON_RAPHSON_X2_P1 = 1.5;
     private static final double NEWTON_RAPHSON_X3_P1 = 3.0;
 
-    private static final double MOD_SECANT_DELTA = 0.01;
+    private static final double MOD_SECANT_DELTA_P1 = 0.01;
 
     //constants for Problem (b)
     private static final double BISECTION_A_P2 = 120;
     private static final double BISECTION_B_P2 = 130;
 
+    private static final double NEWTON_RAPHSON_X_P2 = 130;
+
+    private static final double MOD_SECANT_DELTA_P2 = 0.01;
+
     private static Scanner in;
 
     public static void main(String[] args) {
-        System.out.println("Which function would you like to use to locate zeroes?");
-        System.out.println("1. f(x) = 2(x^3) - 11.7(x^2) + 17.7(x) - 5");
-        System.out.println("2. f(x) = x + 10 - xcosh(50/x)");
 
         in = new Scanner(System.in);
-        RootLocatingMethod [] rlm = null;
 
-        boolean validResp = false;
-        while(!validResp) {
-            System.out.println("Which method would you like to use?");
-            System.out.println("1. Bisection Method");
-            System.out.println("2. Newton-Raphson Method");
-            System.out.println("3. Secant Method");
-            System.out.println("4. False-Position Method");
-            System.out.println("5. Modified Secant Method");
+        while(true) {
+            boolean validResp = false;
+            while(!validResp) {
+                System.out.println("Which function would you like to use to locate zeroes?");
+                System.out.println("1. f(x) = 2(x^3) - 11.7(x^2) + 17.7(x) - 5");
+                System.out.println("2. f(x) = x + 10 - xcosh(50/x)");
+                System.out.println("3. Exit program");
 
-            char choice = in.nextLine().charAt(0);
-            validResp = true;
-            switch(choice){
-                case '1':
-                    rlm = InitBisectionMethod();
-                    break;
-                case '2':
-                    rlm = InitNewtonRaphsonMethod();
-                    break;
-                case '3':
-                    rlm = InitSecantMethod();
-                    break;
-                case '4':
-                    rlm = InitFalsePositionMethod();
-                    break;
-                case '5':
-                    rlm = InitModSecantMethod();
-                    break;
-                default:
-                    System.out.println("Invalid response.");
-                    validResp = false;
+                char choice = in.nextLine().charAt(0);
+                System.out.println();
+                validResp = true;
+                switch(choice){
+                    case '1':
+                        currProblem = problemA;
+                        currProblemPrime = problemAPrime;
+                        break;
+                    case '2':
+                        currProblem = problemB;
+                        currProblemPrime = problemBPrime;
+                        break;
+                    case '3':
+                        System.out.println("Program terminating...");
+                        return;
+                    default:
+                        System.out.println("Invalid response.");
+                        validResp = false;
+                }
             }
-        }
 
-        if(currProblem == problemA) {
+            RootLocatingMethod [] rlm = null;
+
+            validResp = false;
+            while(!validResp) {
+                System.out.println("Which method would you like to use?");
+                System.out.println("1. Bisection Method");
+                System.out.println("2. Newton-Raphson Method");
+                System.out.println("3. Secant Method");
+                System.out.println("4. False-Position Method");
+                System.out.println("5. Modified Secant Method");
+
+                char choice = in.nextLine().charAt(0);
+                System.out.println();
+                validResp = true;
+                switch(choice){
+                    case '1':
+                        rlm = InitBisectionMethod();
+                        break;
+                    case '2':
+                        rlm = InitNewtonRaphsonMethod();
+                        break;
+                    case '3':
+                        rlm = InitSecantMethod();
+                        break;
+                    case '4':
+                        rlm = InitFalsePositionMethod();
+                        break;
+                    case '5':
+                        rlm = InitModSecantMethod();
+                        break;
+                    default:
+                        System.out.println("Invalid response.");
+                        validResp = false;
+                }
+            }
+
             for(int i = 0; i < rlm.length; ++i) {
                 System.out.println("n\tx\t\t\tf(x)\t\t\tapprox. error");
                 // find first zero
@@ -92,11 +123,13 @@ public class Assignment2 {
                 }
                 System.out.println("+++++++++++++");
             }
+            System.out.println();
+            System.out.println();
         }
     }
 
     private static RootLocatingMethod [] InitBisectionMethod() {
-        RootLocatingMethod [] bv = new Bisection[3];
+        RootLocatingMethod [] b = null;
 
         System.out.println("=====Bisection Method=====");
 
@@ -107,15 +140,18 @@ public class Assignment2 {
             System.out.println("2. Enter your own starting points?");
 
             char choice = in.nextLine().charAt(0);
+            System.out.println();
             validResp = true;
             switch(choice){
                 case '1':
                     if(currProblem == problemA) {
-                        bv[0] = new Bisection(BISECTION_A1_P1, BISECTION_B1_P1, currProblem);
-                        bv[1] = new Bisection(BISECTION_A2_P1, BISECTION_B2_P1, currProblem);
-                        bv[2] = new Bisection(BISECTION_A3_P1, BISECTION_B3_P1, currProblem);
+                        b = new Bisection[3];
+                        b[0] = new Bisection(BISECTION_A1_P1, BISECTION_B1_P1, currProblem);
+                        b[1] = new Bisection(BISECTION_A2_P1, BISECTION_B2_P1, currProblem);
+                        b[2] = new Bisection(BISECTION_A3_P1, BISECTION_B3_P1, currProblem);
                     } else {
-
+                        b = new Bisection[1];
+                        b[0] = new Bisection(BISECTION_A_P2, BISECTION_B_P2, currProblem);
                     }
                     break;
                 case '2':
@@ -128,11 +164,11 @@ public class Assignment2 {
             }
         }
 
-        return bv;
+        return b;
     }
 
     private static RootLocatingMethod [] InitNewtonRaphsonMethod() {
-        RootLocatingMethod [] nr = new NewtonRaphson[3];
+        RootLocatingMethod [] nr = null;;
 
         System.out.println("=====Newton-Raphson Method=====");
 
@@ -143,15 +179,18 @@ public class Assignment2 {
             System.out.println("2. Enter your own starting points?");
 
             char choice = in.nextLine().charAt(0);
+            System.out.println();
             validResp = true;
             switch(choice){
                 case '1':
                     if(currProblem == problemA) {
+                        nr = new NewtonRaphson[3];
                         nr[0] = new NewtonRaphson(NEWTON_RAPHSON_X1_P1, currProblem, currProblemPrime);
                         nr[1] = new NewtonRaphson(NEWTON_RAPHSON_X2_P1, currProblem, currProblemPrime);
                         nr[2] = new NewtonRaphson(NEWTON_RAPHSON_X3_P1, currProblem, currProblemPrime);
                     } else {
-
+                        nr = new NewtonRaphson[1];
+                        nr[0] = new NewtonRaphson(NEWTON_RAPHSON_X_P2, currProblem, currProblemPrime);
                     }
                     break;
                 case '2':
@@ -168,7 +207,7 @@ public class Assignment2 {
     }
 
     private static RootLocatingMethod [] InitSecantMethod() {
-        RootLocatingMethod [] s = new Secant[3];
+        RootLocatingMethod [] s = null;;
 
         System.out.println("=====Secant Method=====");
 
@@ -179,15 +218,18 @@ public class Assignment2 {
             System.out.println("2. Enter your own starting points?");
 
             char choice = in.nextLine().charAt(0);
+            System.out.println();
             validResp = true;
             switch(choice){
                 case '1':
                     if(currProblem == problemA) {
+                        s = new Secant[3];
                         s[0] = new Secant(BISECTION_A1_P1, BISECTION_B1_P1, currProblem);
                         s[1] = new Secant(BISECTION_A2_P1, BISECTION_B2_P1, currProblem);
                         s[2] = new Secant(BISECTION_A3_P1, BISECTION_B3_P1, currProblem);
                     } else {
-
+                        s = new Secant[1];
+                        s[0] = new Secant(BISECTION_A_P2, BISECTION_B_P2, currProblem);
                     }
                     break;
                 case '2':
@@ -204,7 +246,7 @@ public class Assignment2 {
     }
 
     private static RootLocatingMethod [] InitFalsePositionMethod() {
-        RootLocatingMethod [] fp = new FalsePosition[3];
+        RootLocatingMethod [] fp = null;;
 
         System.out.println("=====False Position Method=====");
 
@@ -215,15 +257,18 @@ public class Assignment2 {
             System.out.println("2. Enter your own starting points?");
 
             char choice = in.nextLine().charAt(0);
+            System.out.println();
             validResp = true;
             switch(choice){
                 case '1':
                     if(currProblem == problemA) {
+                        fp = new FalsePosition[3];
                         fp[0] = new FalsePosition(BISECTION_A1_P1, BISECTION_B1_P1, currProblem);
                         fp[1] = new FalsePosition(BISECTION_A2_P1, BISECTION_B2_P1, currProblem);
                         fp[2] = new FalsePosition(BISECTION_A3_P1, BISECTION_B3_P1, currProblem);
                     } else {
-
+                        fp = new FalsePosition[1];
+                        fp[0] = new FalsePosition(BISECTION_A_P2, BISECTION_B_P2, currProblem);
                     }
                     break;
                 case '2':
@@ -240,7 +285,7 @@ public class Assignment2 {
     }
 
     private static RootLocatingMethod [] InitModSecantMethod() {
-        RootLocatingMethod [] ms = new ModSecant[3];
+        RootLocatingMethod [] ms = null;;
 
         System.out.println("=====Modified Secant Method=====");
 
@@ -251,15 +296,18 @@ public class Assignment2 {
             System.out.println("2. Enter your own starting points?");
 
             char choice = in.nextLine().charAt(0);
+            System.out.println();
             validResp = true;
             switch(choice){
                 case '1':
                     if(currProblem == problemA) {
-                        ms[0] = new ModSecant(NEWTON_RAPHSON_X1_P1, MOD_SECANT_DELTA, currProblem);
-                        ms[1] = new ModSecant(NEWTON_RAPHSON_X2_P1, MOD_SECANT_DELTA, currProblem);
-                        ms[2] = new ModSecant(NEWTON_RAPHSON_X3_P1, MOD_SECANT_DELTA, currProblem);
-                    } else {
-
+                        ms = new ModSecant[3];
+                        ms[0] = new ModSecant(NEWTON_RAPHSON_X1_P1, MOD_SECANT_DELTA_P1, currProblem);
+                        ms[1] = new ModSecant(NEWTON_RAPHSON_X2_P1, MOD_SECANT_DELTA_P1, currProblem);
+                        ms[2] = new ModSecant(NEWTON_RAPHSON_X3_P1, MOD_SECANT_DELTA_P1, currProblem);
+                    } else /*Problem2*/ {
+                        ms = new ModSecant[1];
+                        ms[0] = new ModSecant(NEWTON_RAPHSON_X_P2, MOD_SECANT_DELTA_P2, currProblem);
                     }
                     break;
                 case '2':
